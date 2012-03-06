13a6bd8523e33c7fbf4518c3101653082f34f7a3
Michael McClenaghan <mike@sideline.ca>
2012-03-06 16:37:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinnieSample::Application.config.secret_token = '99e3ec5cf0370b0c263cd8d5f3cda622c2c4ec5c2ea24528a7484cceb45ea9cc8591482d0384c85d745cbe3293a1afdde4f9a2489b1f89ebe89f0137e9865795'
